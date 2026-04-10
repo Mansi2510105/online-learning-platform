190faa2c4170b678013839c4edad7b024862e3c2
Mansi2510105 <mansisingh24906@gmail.com>
2026-04-11 00:46:05 +0530

    fix error in middleware

--- middleware.js
+++ middleware.js
@@ -1,28 +1,17 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isPublicRoute = createRouteMatcher([
-//   "/",
-//   "/sign-in(.*)",
-//   "/sign-up(.*)"
-// ]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
-// export default clerkMiddleware((auth, req) => {
-//   if (!isPublicRoute(req)) {
-//     auth().protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: ["/((?!_next|.*\\..*).*)"],
-// };
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req) => {
+  if (!isPublicRoute(req)) {
+    return auth().protect();
+  }
+});
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
