87d361b04f4e58020fff35038e1beabcd16e1d9c
Mansi2510105 <mansisingh24906@gmail.com>
2026-04-11 00:05:49 +0530

    update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,22 +1,33 @@
 // 
 
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/'
-])
+// const isPublicRoute = createRouteMatcher([
+//   '/sign-in(.*)',
+//   '/sign-up(.*)',
+//   '/'
+// ])
 
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth.protect()
-  }
-})
+// export default clerkMiddleware((auth, req) => {
+//   if (!isPublicRoute(req)) {
+//     auth.protect()
+//   }
+// })
+
+// export const config = {
+//   matcher: [
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     '/(api|trpc)(.*)',
+//   ],
+// }
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|.*\\..*).*)",
+    "/(api|trpc)(.*)",
   ],
-}
+};
